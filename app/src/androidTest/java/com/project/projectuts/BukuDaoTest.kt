78337c733ab8fd10e3bca8393b62e7684684e38c
bimacakra30 <biimackra@gmail.com>
2024-10-25 10:50:29 +0700

    Update validasi androidTest

--- app/src/androidTest/java/com/project/projectuts/BukuDaoTest.kt
+++ app/src/androidTest/java/com/project/projectuts/BukuDaoTest.kt
@@ -12,6 +12,8 @@ import org.junit.Assert.assertEquals
 import com.project.projectuts.dao.BukuDao
 import com.project.projectuts.database.AplikasiDatabase
 import com.project.projectuts.model.Buku
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertNull
 
 @RunWith(AndroidJUnit4::class)
 class BukuDaoTest {
@@ -39,24 +41,40 @@ class BukuDaoTest {
 
         bukuDao.insertBuku(buku)
 
-        assertEquals(buku.judul,"Kotlin Programming")
+        assertEquals(buku.judul, "Kotlin Programming")
         assertEquals(buku.pengarang, "Bima Cakra")
         assertEquals(buku.tahunTerbit, 2024)
     }
-    @Test
-    fun testDeleteBuku() = runBlocking {
-        val buku = Buku(judul = "Kotlin Programming", pengarang = "Bima Cakra", tahunTerbit = 2024)
-        bukuDao.insertBuku(buku)
 
-        bukuDao.deleteBuku(buku)
-    }
     @Test
     fun testUpdateBuku() = runBlocking {
-        val buku = Buku(judul = "Kotlin Programming", pengarang = "Bima Cakra", tahunTerbit = 2024)
+        val buku = Buku(id = 1, judul = "Kotlin Programming", pengarang = "Bima Cakra", tahunTerbit = 2024)
         bukuDao.insertBuku(buku)
+        val insertedBukuId = buku.id
 
         val updatedBuku = buku.copy(judul = "Kitab Jarkom")
         bukuDao.updateBuku(updatedBuku)
 
+        val retrievedBuku = bukuDao.getBukuById(insertedBukuId)
+        assertEquals("Kitab Jarkom", retrievedBuku?.judul)
+        assertEquals("Bima Cakra", retrievedBuku?.pengarang)
+        assertEquals(2024, retrievedBuku?.tahunTerbit)
     }
+
+    @Test
+    fun testDeleteBuku() = runBlocking {
+        val buku = Buku(id = 1, judul = "Kotlin Programming", pengarang = "Bima Cakra", tahunTerbit = 2024)
+        bukuDao.insertBuku(buku)
+
+        val retrieveBuku = bukuDao.getBukuById(buku.id)
+        assertNotNull(retrieveBuku)
+        assertEquals("Kotlin Programming", retrieveBuku?.judul)
+
+        bukuDao.deleteBuku(buku)
+
+        val deleteBuku = bukuDao.getBukuById(buku.id)
+        assertNull(deleteBuku)
     }
+}
+
+
